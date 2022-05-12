14152d2e4ebc971c056a006047c859eaf9c51e8b
Nytuo <nytuo.yt@gmail.com>
2022-05-12 16:59:26 +0200

    new png

--- js/ncage.js
+++ js/ncage.js
@@ -2,18 +2,16 @@ $(document).ready(function(){
     
     setInterval(function(){
 	$('img').each(function(){
-		console.log('hola cage');
-	    if(!$(this).hasClass("caged")){
-		var number = 1 + Math.floor(Math.random() * 303);
-		if(number < 11){
-		    var image = chrome.extension.getURL("img/"+number+".jpg");
-		}
-		else{
-		    var image = chrome.extension.getURL("img/"+number+".gif");
-		}
+		console.log('REYged');
+	    if(!$(this).hasClass("reyged")){
+		var number = 1 + Math.floor(Math.random() * 77);
+		    var image = chrome.extension.getURL("img/"+number+".png");
+
+			
+
 		var width = $(this).width();
 		var height = $(this).height();
-		$(this).addClass("caged");
+		$(this).addClass("reyged");
 		$(this).attr("src",image);
 		$(this).width(width);
 		$(this).height(height);
